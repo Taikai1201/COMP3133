98c3e2d8d7c6f8df28fe3de2223b96f086ee8b3f
Taikai Vu <vudangdaiduong@gmail.com>
2024-01-21 14:42:49 -0500

    lab1

---
+++ Lab01_101354480_COMP3133/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+function dataFiltrate(inputFileName, outputFileName, filterCountry) {
+  const outputFilePath = `./${outputFileName}.txt`;
+
+  if (fs.existsSync(outputFilePath)) {
+    fs.unlinkSync(outputFilePath);
+  }
+
+  const writeStream = fs.createWriteStream(outputFilePath, { flags: 'a' });
+
+
+  fs.createReadStream(inputFileName)
+    .pipe(csv())
+    .on('data', (row) => {
+  if ('country' in row) {
+            const countryName = row.country.toLowerCase();
+            if (countryName === filterCountry.toLowerCase()) {
+            writeStream.write(`${Object.values(row).join(',')}\n`);
+            }
+        }
+    })
+    .on('end', () => {
+      console.log(`Filtered data for ${filterCountry} has been written to ${outputFileName}.txt`);
+    });
+}
+
+['canada', 'usa'].forEach((country) => {
+  const filePath = `./${country}.txt`;
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+    console.log(`${filePath} has been deleted.`);
+  }
+});
+
+// write to canada.txt
+dataFiltrate('input_countries.csv', 'canada', 'Canada');
+// write to usa.txt
+dataFiltrate('input_countries.csv', 'usa', 'United States');
